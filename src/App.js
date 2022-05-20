67d1bc31454129c38e7d044c5a375d057003da1f
Troy Dorman <troydorman@gmail.com>
2022-05-19 18:32:21 -0600

    added material ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
 
 function App() {
   return (
     <div className="App">
-      
+      <CssBaseline />
+      <Button variant="contained">Hello World</Button>
     </div>
   );
 }
