aed7f6c0f3faed0276e2ba90c246ec318a7c1fe6
Troy Dorman <troydorman@gmail.com>
2022-05-19 21:37:28 -0600

    added submit button

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Box from "@mui/material/Box";
 import { styled } from "@mui/material/styles";
 import Paper from "@mui/material/Paper";
 import { useState } from "react";
+import Button from "@mui/material/Button";
 
 function App() {
   const [count, setCount] = useState(20);
@@ -22,19 +23,18 @@ function App() {
       <CssBaseline />
       <Box sx={{ width: 1 }}>
         <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gap={2}>
-          <Box gridColumn="span 8">
-            <Item>xs=8</Item>
-          </Box>
+          <Box gridColumn="span 8">{/* <Item>xs=8</Item> */}</Box>
           <Box gridColumn="span 4">
             <Item>
               <BasicSelect count={count} />
             </Item>
           </Box>
-          <Box gridColumn="span 4">
-            <Item>xs=4</Item>
-          </Box>
-          <Box gridColumn="span 8">
-            <Item>xs=8</Item>
+          <Box gridColumn="span 4">{/* <Item>xs=4</Item> */}</Box>
+          <Box gridColumn="span 6">{/* <Item>xs=8</Item> */}</Box>
+          <Box gridColumn="span 2">
+            <Item>
+              <Button>Submit</Button>
+            </Item>
           </Box>
         </Box>
       </Box>
